c201dd660e6adedb8d36a5c9dbb29df8515e47b5
Naveen Ramanjulu <nramanjulu97@gmail.com>
2023-06-22 19:20:49 -0500

    swagger api for villa api is up and running!

---
+++ VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VillaAPI.Data;
+using VillaAPI.Models;
+using VillaAPI.Models.Dto;
+
+namespace VillaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/VillaAPI")]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+        // can set response types with eacth endpoint
+        [HttpGet("id:int")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }       
+            var villa = (VillaStore.villaList.FirstOrDefault(u => u.Id == id));
+                if(villa == null) 
+                {
+                    return NotFound();
+                }
+            return Ok(villa);
+            
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO) 
+        {
+            if (villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+            }
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u=>u.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villaDTO);
+
+            return Ok(villaDTO);
+        }
+    }
+}
---
+++ VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using VillaAPI.Models.Dto;
+namespace VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        // returns contents of request in array format
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+            {
+                new VillaDTO { Id = 1, Name = "Pool View" },
+                new VillaDTO { Id = 2, Name = "Beach View" }
+            };
+    }
+}
